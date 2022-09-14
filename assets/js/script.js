e57deee7e90f40c41ce4337aad227a4c30fbacf7
conradjohnson <skaterjames@gmail.com>
2022-09-14 15:55:55 -0500

    working feature using proxy service and proper javascript locally hosted files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,49 @@
+// element variables
+let standingsTable = $('#standings-table-body');
+
+// temporary conference code
 let confCode = "SEC";
-var bearer = 'Bearer ' + 'oVX1IQTlkQCRtifH3Q9YFdibb72wmisO0wrp4tVgiQxgJMCRTGVEdkyWJSKPtj7I';
-fetch('https://api.collegefootballdata.com/records?year=2022&conference=B12', {
-  method: 'GET', //GET is the default.
-  headers:{
-    accept: 'application/json',
-    Authorization: bearer
-  }
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// local end point for local testing
+let localEndPoint = `http://127.0.0.1:3001/records?year=2022&conference=${confCode}`;
+// proxy endpoint nodejs app at our heroku deployment
+let remoteEndPoint = `https://forwarding-app-project-1.herokuapp.com/records?year=2022&conference=${confCode}`;
+
+//function to populate the standings based on the conference code.  
+function populateStandings(){
+    fetch(remoteEndPoint)
+    .then(function (response) {
+        if (response.status===200){
+        return response.json();
+        } else {
+            console.log('here:' + response.text());
+            return response.text();
+        }
+
+    })
+    .then(function (data) {
+        // sort by best conference record
+        data.sort((a, b) => (a.conferenceGames.wins-a.conferenceGames.losses > b.conferenceGames.wins - b.conferenceGames.losses) ? -1 : 1)
+
+        //build table of standings
+        for (let i=0; i<data.length; i++){
+            standingsTable.append(`
+                <tr>
+                    <td data-school="${data[i].team}">${data[i].team}</td>
+                    <td>${data[i].conferenceGames.wins}-${data[i].conferenceGames.losses}</td>
+                    <td>${data[i].total.wins}-${data[i].total.losses}</td>
+                    <td>${data[i].total.games}</td>
+                </tr>
+            
+            `)
+        }
+    });
+
+}
+
+//initialization function to run on page load.
+function init(){
+    populateStandings(); 
+}
+//page is loaded, so let's run our initial code
+init();
+
